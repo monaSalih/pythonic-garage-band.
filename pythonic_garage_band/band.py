ab5e3f2abbaf54793f44ed22e606e47275e2e62d
monaSalih <monaalobisat@gmail.com>
2021-10-13 23:28:55 +0300

    tryFix

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,62 +1,63 @@
 from abc import abstractclassmethod
 class Musician:
     members = []
-
-    def init(self, name):
-
+    def __init__(self, name):
         self.name = name
         Musician.members.append(self)
-
     @abstractclassmethod
-    def str(self):
+    def __str__(self):
         pass
-
     @abstractclassmethod
-    def repr(self):
+    def __repr__(self):
         pass
-
     @abstractclassmethod
     def get_instrument(self):
         pass
-
     @abstractclassmethod
     def play_solo(self):
         pass
 
 class Band:
-    def init(self,name,members,inst=''):
+    def __init__(self,name,members,inst=''):
         self.name=name
         self.members=members
     def play_solo(self):
         return self.inst
 
 class Guitarist(Band):
-    def init(self,name,inst=''):
+    def __init__(self,name,inst=''):
         self.name=name
-    def str(self):
+    def __str__(self):
         return f"My name is {self.name} and I play guitar"
-    def repr(self):
+    def __repr__(self):
         return f"Guitarist instance. Name = {self.name}"
     def get_instrument(self):
         return "guitar"
 
 class Drummer(Band):
-    def init(self,name,inst='rattle boom crash'):
+    def __init__(self,name,inst='rattle boom crash'):
         self.name=name
-    def str(self):
+    def __str__(self):
        return f"My name is {self.name} and I play drums"
-    def repr(self):
+    def __repr__(self):
         return f"Drummer instance. Name = {self.name}"
     def get_instrument(self):
         return "drums"
 
 
 class Bassist(Band):
-    def init(self,name,inst='bom bom buh bom'):
+    def __init__(self,name,inst='bom bom buh bom'):
         self.name=name
-    def str(self):
+    def __str__(self):
        return f"My name is {self.name} and I play bass"
-    def repr(self):
+    def __repr__(self):
         return f"Bassist instance. Name = {self.name}"
     def get_instrument(self):
         return "bass"
+
+        
+        
+
+
+    
+    
