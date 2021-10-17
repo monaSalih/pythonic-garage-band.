ba8261c488933f5749ccf53b44f02b07c2d144ce
monaSalih <monaalobisat@gmail.com>
2021-10-17 14:10:59 +0300

    band2 commit

--- pythonic_garage_band/band2.py
+++ pythonic_garage_band/band2.py
@@ -1,9 +1,9 @@
-from band import (Guitarist,Drummer,Bassist)
-if __name__ == "__main__":
-    Joan = Guitarist('Joan Jett')
-    Sheila = Drummer('Sheila E.')
-    Meshell = Bassist('Meshell Ndegeocello')
+# from band import (Guitarist,Drummer,Bassist)
+# if __name__ == "__main__":
+#     Joan = Guitarist('Joan Jett')
+#     Sheila = Drummer('Sheila E.')
+#     Meshell = Bassist('Meshell Ndegeocello')
 
-    print(Joan)
-    print(Sheila)
-    print(Meshell)
+#     print(Joan)
+#     print(Sheila)
+#     print(Meshell)
